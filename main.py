bbf0d6a2a7c702da97d124c7b47e4680af6584a4
axelcall123 <axel15calderon@gmail.com>
2020-08-21 19:32:34 -0600

    config, pagin html con css

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
-#escribe-html-2-windows.py
+#--------------------------crea pagina web
+import webbrowser
+#---------------------------carga n archivos
+comando = input().upper()# entrada de teclado mayuscula
+#print(comando)
+#salir = true
+if comando == 'CARGAR':
+    print("cargando...")
+elif comando == 'SELECCIONAR':
+    print("seleccionando...")
+else:
+    print("NEL")
+
+#f = open('holamundo.html','w')
 
 import webbrowser
 
-f = open('holamundo.html','wb')
+f = open('holamundo.html','w')
 
-mensaje = """<html>
-<head></head>
-<body><p>Hola Mundo!</p></body>
+mensaje = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/css.css">
+  <title>Document</title>
+</head><body>"""
+mensaje2= comando
+mensaje3= """
+</body>
 </html>"""
 
 f.write(mensaje)
+f.write(mensaje2)
+f.write(mensaje3)
 f.close()
-
 webbrowser.open_new_tab('holamundo.html')
